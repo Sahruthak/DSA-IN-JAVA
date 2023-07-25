c049ea7916a07074509482a11422dfa4190e2b41
SahruthaReddy <107304838+Sahruthak@users.noreply.github.com>
2023-07-25 11:22:25 +0530

    BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,9 +1,17 @@
 class BinarySearch {
     public int search(int[] nums, int target) {
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == target)
-                return i;
-        }
+        int left=0, right = nums.length-1;
+        int mid;
+        while(left <= right){
+            mid = (left+right)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(target < nums[mid]) // if target is on left
+                right = mid-1;
+            else // if target is on right
+                left = mid+1;
+        } 
+        // loop finished, element not found
         return -1;
     }
 }
