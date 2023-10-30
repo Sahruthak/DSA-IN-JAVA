269e07764653b00904789c5775f34d5ca973db99
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2023-10-30 09:32:47 +0530

    Update LL.java

--- Linkedlist/LL.java
+++ Linkedlist/LL.java
@@ -37,6 +37,22 @@ class LinkedList {
         }
     }
 
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+    
     public void display() {
         Node current = head;
         while (current != null) {
