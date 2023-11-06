3430e6bfc13e3127afc305d204fade992720f253
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2023-11-06 23:46:45 +0530

    Create DoublyLinkedList.java

---
+++ Linkedlist/DoublyLinkedList.java
@@ -0,0 +1,100 @@
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // Add a node to the end of the list
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Add a node to the front of the list
+    public void prepend(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    // Delete a node with a specific value
+    public void delete(int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                } else {
+                    tail = current.prev;
+                }
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    // Display the list from the head to tail
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    // Display the list from the tail to head
+    public void displayReverse() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.prepend(0);
+        list.display(); // Output: 0 1 2 3
+        list.displayReverse(); // Output: 3 2 1 0
+        list.delete(2);
+        list.display(); // Output: 0 1 3
+    }
+}
